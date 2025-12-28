ddf58432eca941b9807e48529a7bf5cdaf427ec7
carlos8888-lab <145282595+carlos8888-lab@users.noreply.github.com>
2025-12-28 20:15:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -948,5 +948,5 @@ def create_app() -> Flask:
 app = create_app()
 if __name__ == "__main__":
     puerto = int(os.environ.get("PORT", "5000"))
-
-    app.run(host="0.0.0.0", port=puerto, debug=True)
+    app.run(host="0.0.0.0", port=puerto, debug=False)
+    # app.run(host="0.0.0.0", port=puerto, debug=True)
